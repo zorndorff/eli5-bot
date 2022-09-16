f2aab7d2893f285ba3146801689f105d82ecbe4f
Zac Orndorff <zorndorff@gmail.com>
2022-09-16 10:09:06 -0500

    updated

--- eli5.mjs
+++ eli5.mjs
@@ -1,5 +1,13 @@
-const { Configuration, OpenAIApi } = require("openai");
-const { parseArgs } = require("node:util");
+import { Configuration, OpenAIApi } from "openai";
+import { parseArgs } from "node:util";
+import path from "path";
+import fs from "fs";
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,16 +19,19 @@ const {
   values: { documentPath, preamble },
 } = parseArgs({
   options: {
-    documetPath: {
+    documentPath: {
       type: "string",
       short: "d",
     },
     preamble: {
-      type: "preamble",
+      type: "string",
       short: "p",
     },
   },
 });
+
+console.log(`doc path: ${documentPath}`);
+console.log(`preamble: ${preamble}`);
 
 const docText = fs.readFileSync(path.join(__dirname, documentPath)).toString();
 
@@ -28,13 +39,15 @@ const docText = fs.readFileSync(path.join(__dirname, documentPath)).toString();
 const response = await openai.createCompletion({
   model: "text-davinci-002",
   prompt: `
-    ${prompt}
+    ${preamble}
     ${docText}
-  `  
+  `,
   temperature: 0.7,
-  max_tokens: 1875,
+  max_tokens: 500,
   top_p: 1,
   best_of: 2,
   frequency_penalty: 0,
   presence_penalty: 0,
 });
+
+console.log(JSON.stringify(response.data));
